f4761e81f41e9d411a1290cbbc60389438415ae5
Zaki Al Faridzi <zalfaridzi9@gmail.com>
2021-08-19 09:40:32 +0700

    adding firestore, config chakraui

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,39 @@
-import { ProvideAuth } from "../lib/auth";
-import "../styles/globals.css";
+import { AuthProvider } from "@/lib/auth";
+import { ChakraProvider, CSSReset } from "@chakra-ui/react";
+import { css, Global } from "@emotion/react";
+import { customTheme } from "@/styles/customTheme";
+// import "@/styles/globals.css";
+
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            min-width: 360px;
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ProvideAuth>
-      <Component {...pageProps} />
-    </ProvideAuth>
+    <ChakraProvider theme={customTheme}>
+      <AuthProvider>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ChakraProvider>
   );
 }
 
